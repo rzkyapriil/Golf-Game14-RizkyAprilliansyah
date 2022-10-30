4a1a90e210919cd375e3d61d278239e84ebef1d4
rzkyapriil <rzkyapriil@gmail.com>
2022-10-30 23:05:12 +0700

    Add File

---
+++ Assets/SceneLoader.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    private static string sceneToLoad;
+    public static string SceneToLoad {get => sceneToLoad;}
+
+    // load
+    public static void Load(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+    public static void ProgressLoad(string sceneName)
+    {
+        sceneToLoad = sceneName;
+        SceneManager.LoadScene("LoadingProgress");
+    }
+
+    public static void ReloadLevel()
+    {
+        var currentScene = SceneManager.GetActiveScene().name;
+        ProgressLoad(currentScene);
+    }
+
+    public static void LoadNextLevel()
+    {
+        string currentSceneName = SceneManager.GetActiveScene().name;
+        int nextLevel = int.Parse(currentSceneName.Split("Level")[1]) + 1;
+        string nextSceneName = "Level" + nextLevel;
+
+        if(SceneUtility.GetBuildIndexByScenePath(nextSceneName) == -1)
+        {
+            Debug.LogError(nextSceneName + " does not exists");
+            return;
+        }
+
+        ProgressLoad(nextSceneName);
+    }
+}
